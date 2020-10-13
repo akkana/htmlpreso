50c1f219b435a69d637bdbbb45a4ea5d589ea4ce
Akkana Peck <akkana@shallowsky.com>
2020-10-13 15:06:05 -0600

    presonotes.py can now get image/video notes out of notes.js. But only if you use a format that's a lot like the one in the distribution.

--- presonotes.py
+++ presonotes.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 # Go through an htmlpreso (as in https://github.com/akkana/htmlpreso)
-# and extract all the notes, in order, producing a printable file
-# for emergencies.
+# and extract all the notes, in order, printing to stdout
+# an HTML file you can save or print in case of emergency.
 
 import re
 import sys, os
@@ -24,23 +24,33 @@ def comment_remover(text):
     return re.sub(pattern, replacer, text)
 
 
-def parse_slldelist(filename):
-    wholefile = open(filename).read()
+def remove_quotes_and_comma(s):
+    if s.endswith(','):
+        s = s[:-1]
+    if s.startswith('"') or s.startswith('"'):
+        s = s[1:]
+    if s.endswith('"') or s.startswith('"'):
+        s = s[:-1]
+    return s
 
-    # Remove everything up to "var slides = new Array ("
-    if "var slides = new Array (" not in wholefile:
-        raise RuntimeError("Missing expected 'var slides' line")
-    wholefile = re.sub(re.compile(r'.*var slides = new Array \( *\n',
+def clean_lines(wholefile, start_pat, end_pat):
+    """Remove everything before the start and after the end pattern
+       (including the patterns themselves), and comments and whitespace.
+       Return a dictionary (if there are colons) or list (if not)
+       of what's left in each line.
+    """
+    # Remove everything up to and including the start_pat:
+    wholefile = re.sub(re.compile(start_pat,
+                                  re.DOTALL | re.MULTILINE),
+                       "", wholefile)
+
+    # and from the end_pat to the end of the file:
+    wholefile = re.sub(re.compile(end_pat,
                                   re.DOTALL | re.MULTILINE),
                        "", wholefile)
 
     # Remove C-style comments
     wholefile = comment_remover(wholefile)
-
-    # Remove the closing ); and everything after
-    # wholefile = re.sub(re.compile(r"\n\);\n.*", re.MULTILINE), "", wholefile)
-    wholefile = re.sub(re.compile(r'\n\);\n.*$', re.DOTALL | re.MULTILINE),
-                       "", wholefile)
 
     # Remove blank lines. This doesn't work, but it's easy enough
     # to ignore them later.
@@ -48,24 +58,40 @@ def parse_slldelist(filename):
     #                   "\n", wholefile)
     # print("=========== Removed blank lines:", wholefile)
 
-    slidelist = []
+    # Loop over lines, removing whitespace and quotes:
+    linelist = None
     for line in wholefile.split('\n'):
         line = line.strip()
         if not line:
             continue
-        if line.endswith(','):
-            line = line[:-1]
-        if line.startswith('"') or line.startswith('"'):
-            line = line[1:]
-        if line.endswith('"') or line.startswith('"'):
-            line = line[:-1]
-        slidelist.append(line)
+        if ':' in line:
+            if not linelist:
+                linelist = {}
+            parts = [ remove_quotes_and_comma(p.strip())
+                                              for p in line.split(':') ]
+            linelist[parts[0]] = parts[1]
+        else:
+            if not linelist:
+                linelist = []
+            line = remove_quotes_and_comma(line)
+            linelist.append(line)
 
-    return slidelist
+    return linelist
 
+
+def parse_slldelist(filename):
+    return clean_lines(open(filename).read(),
+                       start_pat=r'.*var slides = new Array \( *\n',
+                       end_pat=r"\n\);\n.*$")
+
+notelist = None
 
 def parse_notes_file():
-    allnotestxt = open('notes.js').read()
+    global notelist
+
+    notelist = clean_lines(open("notes.js").read(),
+                           start_pat=r'.*var notes = {\n',
+                           end_pat=r'\n\}\n.*$')
 
 
 def find_notes_in_file(filename):
@@ -80,14 +106,29 @@ def find_notes_in_file(filename):
 
 
 def find_notes(slide):
+    if not notelist:
+        parse_notes_file()
+
     if os.path.exists(slide):
         return find_notes_in_file(slide)
     else:
         if '?' not in slide:
-            print(slide, "is not a file or img/video, not sure what to do")
-            return
+            return f"{slide} is not a file or img/video, not sure what to do"
+
         slide = re.sub(r'.*\?', '', slide)
-        return(f"want to find notes for {slide}")
+
+        try:
+            # First try the whole thing including title
+            return notelist[slide]
+        except KeyError:
+            pass
+
+        # Strip off title and try again
+        shortslide = re.sub("\&title=.*", "", slide)
+        try:
+            return notelist[shortslide]
+        except KeyError:
+            return f"No notes for either {slide} or {shortslide}"
 
 
 if __name__ == '__main__':
